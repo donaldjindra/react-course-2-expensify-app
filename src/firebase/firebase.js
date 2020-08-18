71831c5f37cabfadde80e7e2a6d7405994ee0fb0
Donald <djindra@gmail.com>
2020-08-17 22:16:55 -0700

    Setup test database environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,35 +1,18 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsP4qYV4f1RzHX7RSRV-zOmJC29zGAD24",
-    authDomain: "expensify-a06a0.firebaseapp.com",
-    databaseURL: "https://expensify-a06a0.firebaseio.com",
-    projectId: "expensify-a06a0",
-    storageBucket: "expensify-a06a0.appspot.com",
-    messagingSenderId: "977695825544",
-    appId: "1:977695825544:web:d9a94aa204954bb5ea093d",
-    measurementId: "G-B2WGMR8K5Y"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
   firebase.initializeApp(firebaseConfig);
 
   const database = firebase.database()
 
-  database.ref().set({
-      name: 'Donald Jindra',
-      age: 43,
-      isSingle: true,
-      location: {
-          city: 'Los Angeles',
-          country: 'United States'
-      }
-  });
-
-//   database.ref().set('This is my data')
-
-database.ref('age').set(44);
-database.ref('location/city').set('New York');
-database.ref('attributes').set({
-    height: 70,
-    weight: 135
-});
+  export { firebase, database as default };
--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,17 +1,25 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(()=> {
-        // resolve({
-        //     name: 'Donald',
-        //     age: 43
-        // });
-        reject('Something went wrong')
+        resolve({
+            name: 'Donald',
+            age: 43
+        });
+        // reject('Something went wrong')
     }, 1500)
 });
 
 console.log('Before')
 
 promise.then((data) =>{
-    console.log(data);
+    console.log('1', data);
+
+    return new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        resolve('This is my other promise');
+    }, 1500)
+});
+}).then((str) => {
+    console.log('does this run?', str)
 }).catch((error) => {
     console.log('Error:', error)
 });
